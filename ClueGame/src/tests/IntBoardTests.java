c9c44078b4a5fa63da8cc49fededf4a177f200cc
Shannon Bride <sbride@mines.edu>
2018-03-01 12:50:42 -0700

    fixed issue with null point exception- assigned new cells to identical existing cells

---
+++ ClueGame/src/tests/IntBoardTests.java
@@ -0,0 +1,48 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import experiment.BoardCell;
+import experiment.IntBoard;
+
+public class IntBoardTests {
+	IntBoard board;
+	
+//	@Before
+//	public void beforeAll(){
+//		IntBoard board = new IntBoard();
+//	}
+
+	
+	@Test
+	public void testAdjacency0(){
+		board = new IntBoard();
+		BoardCell cell = board.getCell(0,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());		
+	}
+	
+	@Test
+	public void testTargets0_3()
+	{
+		board = new IntBoard();
+		BoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set targets = board.getTargets();
+		assertEquals(6, targets.size());
+		assertTrue(targets.contains(board.getCell(2, 0)));
+		assertTrue(targets.contains(board.getCell(2, 1)));
+		assertTrue(targets.contains(board.getCell(0, 1)));
+		assertTrue(targets.contains(board.getCell(1, 2)));
+		assertTrue(targets.contains(board.getCell(0, 3)));
+		assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+}
