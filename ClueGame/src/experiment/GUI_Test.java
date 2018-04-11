90e2771c060886db0c57b102bbca18fa5878ca0d
Stephen Kistler <skistler@mymail.mines.edu>
2018-04-10 23:33:16 -0600

    Finished most of GUI_Test

---
+++ ClueGame/src/experiment/GUI_Test.java
@@ -0,0 +1,66 @@
+package experiment;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI_Test extends JPanel{
+	private JTextField turn;
+	private JTextField dieRoll;
+	private JTextField guess;
+	private JTextField guessResult;
+	
+	public GUI_Test(){
+		setLayout(new GridLayout(6,5));
+		createLayout();
+		
+	}
+	
+	public void createLayout(){
+		JLabel turnLabel = new JLabel("Whose Turn:");
+		turn = new JTextField(5);
+		add(turnLabel, BorderLayout.EAST);
+		add(turn, BorderLayout.EAST);
+		
+		JLabel dieRollLabel = new JLabel("Die Roll:");
+		dieRoll = new JTextField(5);
+		add(dieRollLabel, BorderLayout.EAST);
+		add(dieRoll, BorderLayout.EAST);
+		
+		JLabel guessLabel = new JLabel("Guess:");
+		guess = new JTextField(5);
+		add(guessLabel, BorderLayout.EAST);
+		add(guess, BorderLayout.EAST);
+		
+		JLabel guessResultLabel = new JLabel("Guess Result:");
+		guessResult = new JTextField(5);
+		add(guessResultLabel, BorderLayout.EAST);
+		add(guessResult, BorderLayout.EAST);
+		
+		JButton nextPlayer = new JButton("Next Player");
+		JButton makeAccusation = new JButton("Make an Accusation");
+		add(makeAccusation, BorderLayout.EAST);
+		add(nextPlayer, BorderLayout.WEST);
+		
+	}
+	
+	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("GUI Example");
+		frame.setSize(250, 150);
+		
+		GUI_Test gui = new GUI_Test();
+		frame.add(gui, BorderLayout.CENTER);
+		
+		frame.setVisible(true);
+	}
+}
